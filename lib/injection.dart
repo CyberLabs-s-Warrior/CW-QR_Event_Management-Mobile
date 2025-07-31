175a3167fb6365d5825733df693c128b7058ac51
nabildzr <muhammadnbillz@gmail.com>
2025-07-31 13:54:03 +0700

    refactor(injection): reorganize dependency injection for authentication features and remove unused provider imports

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,14 +1,49 @@
 import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'core/provider/validation_provider.dart';
+import 'features/Authentication/data/datasources/remote_datasource.dart';
+import 'features/Authentication/data/repositories/authentication_repository_impl.dart';
+import 'features/Authentication/domain/repositories/authentication_repository.dart';
+import 'features/Authentication/domain/usecases/forgot_password.dart';
+import 'features/Authentication/domain/usecases/sign_in.dart';
+import 'features/Authentication/domain/usecases/verify_code.dart';
+import 'features/Authentication/presentation/provider/authentication_provider.dart';
 
-var myInjection = GetIt.instance;
+final GetIt myInjection = GetIt.instance;
 
 Future<void> init() async {
-  myInjection.registerLazySingleton(
-    () => MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ValidationProvider())],
+  // External dependencies
+  final sharedPreferences = await SharedPreferences.getInstance();
+  myInjection.registerLazySingleton(() => sharedPreferences);
+  myInjection.registerLazySingleton(() => http.Client());
+
+  // Data sources
+  myInjection.registerLazySingleton<AuthenticationRemoteDataSource>(
+    () => AuthenticationRemoteDataSourceImplementation(
+      client: myInjection(),
+    ),
+  );
+
+  // Repository
+  myInjection.registerLazySingleton<AuthenticationRepository>(
+    () => AuthenticationRepositoryImpl(
+      authenticationRemoteDataSource: myInjection(),
+      sharedPreferences: myInjection(),
+    ),
+  );
+
+  // Use Cases
+  myInjection.registerLazySingleton(() => SignIn(myInjection()));
+  myInjection.registerLazySingleton(() => ForgotPassword(myInjection()));
+  myInjection.registerLazySingleton(() => VerifyCode(myInjection()));
+
+  // Providers
+  myInjection.registerFactory(
+    () => AuthenticationProvider(
+      signInUseCase: myInjection(),
+      forgotPasswordUseCase: myInjection(),
+      verifyCodeUseCase: myInjection(),
     ),
   );
 }
