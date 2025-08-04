7f5abaf921dde54ccfd84a59686071ae3937ea15
nabildzr <muhammadnbillz@gmail.com>
2025-08-04 09:05:18 +0700

    refactor(snack_bar): update import statement for consistency

--- lib/gen/alert/snack_bar.dart
+++ lib/gen/alert/snack_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qr_event_management/core/theme/app_colors.dart';
+import '../../core/theme/app_colors.dart';
 
 void showCustomSnackBar({
   required BuildContext context,
