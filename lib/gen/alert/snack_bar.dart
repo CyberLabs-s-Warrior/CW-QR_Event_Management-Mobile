51cd9c9b1115862c4e65edf9e568e8c44ee3303b
nabildzr <muhammadnbillz@gmail.com>
2025-08-04 09:03:48 +0700

    feat(snack_bar, dialog_screen): add custom snack bar and loading dialog functions for improved user feedback

---
+++ lib/gen/alert/snack_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:qr_event_management/core/theme/app_colors.dart';
+
+void showCustomSnackBar({
+  required BuildContext context,
+  required String message,
+  Color? color,
+}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      dismissDirection: DismissDirection.up,
+      content: Text(message),
+      backgroundColor: color ?? AppColors.primary,
+      margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height - 100,
+        left: 10,
+        right: 10,
+      ),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+  );
+}
