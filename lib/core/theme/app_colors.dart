55334bbccfc271275a89ccb2ec700c72c9511d1a
nabildzr <muhammadnbillz@gmail.com>
2025-08-07 21:13:42 +0700

    refactor: update color definitions in AppColors class

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF3F7CFF);
-  static const Color primaryLight = Color(0xFFABC8FE);
-  static const Color secondary = Color(0xFFF1F6FF);
+  static const Color primaryLight = Color(0xffc7daff);
+  static const Color secondary = Color(0xffdde9ff);
+  static const Color third = Color(0xffABC8FE);
+
 
   static const Color white = Colors.white;
   static const Color grey = Colors.grey;
   static const Color black = Colors.black;
+
+  static const Color backgroundPage = Colors.white;
 
   static const Color success = Color(0xFF4CAF50);
   static const Color error = Color(0xFFE53E3E);
